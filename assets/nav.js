4731b95b413050b6ed32aed7fba222f8649d3a88
avni jain <javni647@gmail.com>
2019-11-01 18:14:31 +0530

    2

--- assets/nav.js
+++ assets/nav.js
@@ -1,109 +1,14 @@
-// $('document').ready(function() {
-//
-//      // We need to duplicate the whole body of the website so if you scroll down you can see both the bottom and the top at the same time. Before we do this we need to know the original height of the website.
-//      var origDocHeight = document.body.offsetHeight;
-//
-//      // now we know the height we can duplicate the body
-//      $("body").contents().clone().appendTo("body");
-//
-//
-//      $(document).scroll(function(){ // detect scrolling
-//
-//          var scrollWindowPos = $(document).scrollTop(); // store how far we have scrolled
-//
-//          if(scrollWindowPos >= origDocHeight ) { // if we scrolled further then the original doc height
-//              $(document).scrollTop(0); // then scroll to the top
-//          }
-//      });
-//
-//  });
-
-       // var doc = window.document,
-//   context = doc.querySelector('.js-loop'),
-//   clones = context.querySelectorAll('.is-clone'),
-//   disableScroll = false,
-//   scrollHeight = 0,
-//   scrollPos = 0,
-//   clonesHeight = 0,
-//   i = 0;
-//
-// function getScrollPos () {
-//   return (context.pageYOffset || context.scrollTop) - (context.clientTop || 0);
-// }
-//
-// function setScrollPos (pos) {
-//   context.scrollTop = pos;
-// }
-//
-// function getClonesHeight () {
-//   clonesHeight = 0;
-//
-//   for (i = 0; i < clones.length; i += 1) {
-//     clonesHeight = clonesHeight + clones[i].offsetHeight;
-//   }
-//
-//   return clonesHeight;
-// }
-//
-// function reCalc () {
-//   scrollPos = getScrollPos();
-//   scrollHeight = context.scrollHeight;
-//   clonesHeight = getClonesHeight();
-//
-//   if (scrollPos <= 0) {
-//     setScrollPos(1); // Scroll 1 pixel to allow upwards scrolling
-//   }
-// }
-//
-// function scrollUpdate () {
-//   if (!disableScroll) {
-//     scrollPos = getScrollPos();
-//
-//     if (clonesHeight + scrollPos >= scrollHeight) {
-//       // Scroll to the top when you’ve reached the bottom
-//       setScrollPos(1); // Scroll down 1 pixel to allow upwards scrolling
-//       disableScroll = true;
-//     } else if (scrollPos <= 0) {
-//       // Scroll to the bottom when you reach the top
-//       setScrollPos(scrollHeight - clonesHeight);
-//       disableScroll = true;
-//     }
-//   }
-//
-//   if (disableScroll) {
-//     // Disable scroll-jumping for a short time to avoid flickering
-//     window.setTimeout(function () {
-//       disableScroll = false;
-//     }, 40);
-//   }
-// }
-//
-// function init () {
-//   reCalc();
-//
-//   context.addEventListener('scroll', function () {
-//     window.requestAnimationFrame(scrollUpdate);
-//   }, false);
-//
-//   window.addEventListener('resize', function () {
-//     window.requestAnimationFrame(reCalc);
-//   }, false);
-// }
-//
-// if (document.readyState !== 'loading') {
-//   init()
-// } else {
-//   doc.addEventListener('DOMContentLoaded', init, false)
-// }
-//
-//
-//
-//
-//
-//
-//
-//
-// // Just for this demo: Center the middle block on page load
-// window.onload = function () {
-//   setScrollPos(Math.round(clones[0].getBoundingClientRect().top + getScrollPos() - (context.offsetHeight - clones[0].offsetHeight) / 2));
-// };
+$(function() {
+    var img = $('.rick').html();
+    for (var i = 0; i < 10; i++) {
+        moar(img);
+    }
+    $(window).on('scroll', function() {
+        if ($(this).scrollTop() >= $('.rick').height() - $(this).height()) {
+            moar(img);
+        }
+    });
+    function moar(content) {
+        $('.rick').append(content);
+    }
+});
